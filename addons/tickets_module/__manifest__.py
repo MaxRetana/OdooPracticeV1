b3d02b49fed946297fa8883d62ef679fa844bd0b
Max Retana <maxretana981@gmail.com>
2025-02-05 17:45:58 -0600

    Adding assigned user

--- addons/tickets_module/__manifest__.py
+++ addons/tickets_module/__manifest__.py
@@ -1,11 +1,12 @@
 {
   'name': 'The Tickets Module',
-  'version': '1.0',
+  'version': '2.0',
   'author': 'Maximiliano Retana',
+  'license': 'LGPL-3',  # Agrega una licencia (LGPL-3 es común para módulos de Odoo)
   'depends': ['base'],
   'data': [
     'views/tickets_module_view.xml',
-    'views/security.xml'
+    'views/security.xml',
   ],
   'installable': True,
   'auto_install': False,
--- addons/tickets_module/models/tickets_module.py
+++ addons/tickets_module/models/tickets_module.py
@@ -1,15 +1,15 @@
 from odoo import models, fields, api
 
 class TicketsModule(models.Model):
-  _name = 'tickets_module.tickets_module'
-  _description = 'tickets_module.tickets_module'
-  
-  
-  name = fields.Char(string='Name', required=True)
-  description = fields.Text(string='Description')
-  date_open = fields.Datetime(string='Date')
-  date_closed = fields.Datetime(string='Date')
-  status = fields.Selection([
-    ('open', 'Open'),
-    ('closed', 'Closed')
-  ], string='Status', default='open')
+    _name = 'tickets_module.tickets_module'
+    _description = 'tickets_module.tickets_module'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    date_open = fields.Datetime(string='Date Opened')
+    date_closed = fields.Datetime(string='Date Closed')
+    status = fields.Selection([
+        ('open', 'Open'),
+        ('closed', 'Closed')
+    ], string='Status', default='open')
+    assigned_user_id = fields.Many2one('res.users', string='Assigned User')
