a021f2d6bdf6687a89035e08a3b177a16154cc10
Max Retana <maxretana981@gmail.com>
2025-02-05 10:29:13 -0600

    Module Tickets Done

---
+++ addons/tickets_module/__init__.py
@@ -0,0 +1,1 @@
+from .models import tickets_module
---
+++ addons/tickets_module/__manifest__.py
@@ -0,0 +1,13 @@
+{
+  'name': 'The Tickets Module',
+  'version': '1.0',
+  'author': 'Maximiliano Retana',
+  'depends': ['base'],
+  'data': [
+    'views/tickets_module_view.xml',
+    'views/security.xml'
+  ],
+  'installable': True,
+  'auto_install': False,
+  'application': True,
+}
